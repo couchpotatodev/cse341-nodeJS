a98a6e55a92b8a4d0b187a88c1a62db4bd5ec73e
couchpotatodev <mjrimayaho@gmail.com>
2022-01-19 23:19:20 +0800

    update

--- prove/routes/admin.js
+++ prove/routes/admin.js
@@ -2,51 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const productsArray = [];
-
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-}); 
-/*router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    title: 'Add Product',
-    products: productsArray,
-    path: '/add-product', // For pug, EJS
-    //activeTA03: true, // For HBS
-    //contentCSS: true, // For HBS
-  });
-});
-*/
-
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => POST
-/*router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-}); */
-
-router.post('/add-product', (req, res, next) => {
-  const newProduct = req.body.newProduct;
-
-  userArray.push(newProduct);
-
-  res.redirect('/');
-});
-
-
-router.get('/', (req, res, next) => {
-  res.render('views/shop', {
-    title: 'Shop',
-    products: productsArray,
-    path: '/shop', // For pug, EJS
-    //activeTA03: true, // For HBS
-    //contentCSS: true, // For HBS
-  });
-});
+router.post('/add-product', productsController.postAddProduct);
 
 module.exports = router;
