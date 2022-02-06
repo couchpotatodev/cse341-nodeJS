bc857c250966a3cecf6b91b3aa63730c2bd4dc48
couchpotatodev <mjrimayaho@gmail.com>
2022-02-06 23:08:23 +0800

    updated files
    edited files

--- prove/app.js
+++ prove/app.js
@@ -3,13 +3,24 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+
 const PORT = process.env.PORT || 5000
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
-const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+const MONGODB_URI = process.env.MONGODB_URL || "mongodb+srv://tonski:hstHS1um8UJxHaBj@cluster0.dx2uj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
+const csrfProtection = csrf();
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
 const corsOptions = {
     origin: "https://mysarisaristore.herokuapp.com/",
     optionsSuccessStatus: 200
@@ -24,19 +35,36 @@ const options = {
     family: 4
 };
 
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://tonski:hstHS1um8UJxHaBj@cluster0.dx2uj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+//const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://tonski:hstHS1um8UJxHaBj@cluster0.dx2uj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+
+app.use(csrfProtection);
+app.use(flash());
+
 
 app.use((req, res, next) => {
-  User.findById('61f62cdbe5e9ec31cc667453')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
@@ -44,14 +72,29 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
+/*app.use((req, res, next) => {
+  User.findById('61f62cdbe5e9ec31cc667453')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+*/
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
+app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
   .connect(
-    MONGODB_URL, options
+    MONGODB_URI, options
   )
   .then(result => {
     User.findOne().then(user => {
